2abd4fa84eac1ce9fa75304ee49c3b5a8c2cbc75
dsssssssss9 <30810847+dsssssssss9@users.noreply.github.com>
2020-10-14 23:39:18 +0100

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -9,6 +9,7 @@ basic.showLeds(`
 basic.pause(100)
 basic.forever(function () {
     led.plot(randint(0, 4), randint(0, 4))
+    basic.pause(50)
     led.unplot(randint(0, 4), randint(0, 4))
     basic.pause(50)
 })
