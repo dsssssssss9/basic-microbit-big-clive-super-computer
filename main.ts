b8a223c6c22997f8e435ca09f708f5549beef7d4
dsssssssss9 <30810847+dsssssssss9@users.noreply.github.com>
2020-10-15 00:02:05 +0100

    set up & use  delay variable

--- main.ts
+++ main.ts
@@ -1,3 +1,4 @@
+let Delay = 20
 led.setDisplayMode(DisplayMode.Greyscale)
 basic.clearScreen()
 basic.showLeds(`
@@ -10,8 +11,8 @@ basic.showLeds(`
 basic.pause(100)
 basic.forever(function () {
     led.plotBrightness(randint(0, 4), randint(0, 4), randint(0, 255))
-    basic.pause(randint(0, 20))
+    basic.pause(randint(0, Delay))
     led.unplot(randint(0, 4), randint(0, 4))
-    basic.pause(randint(0, 20))
+    basic.pause(randint(0, Delay))
     led.toggle(randint(0, 4), 0)
 })
