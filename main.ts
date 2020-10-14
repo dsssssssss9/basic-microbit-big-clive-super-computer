ef4c08017a176df33bfcbed719178588356ba031
dsssssssss9 <30810847+dsssssssss9@users.noreply.github.com>
2020-10-14 23:41:57 +0100

    decresed delay

--- main.ts
+++ main.ts
@@ -9,7 +9,7 @@ basic.showLeds(`
 basic.pause(100)
 basic.forever(function () {
     led.plot(randint(0, 4), randint(0, 4))
-    basic.pause(randint(0, 50))
+    basic.pause(randint(0, 20))
     led.unplot(randint(0, 4), randint(0, 4))
-    basic.pause(randint(0, 50))
+    basic.pause(randint(0, 20))
 })
