6cb5813be18b7d72282170b4341f8a4848b7bb24
dsssssssss9 <30810847+dsssssssss9@users.noreply.github.com>
2020-10-14 23:45:24 +0100

    plot using random brightness

--- main.ts
+++ main.ts
@@ -8,8 +8,9 @@ basic.showLeds(`
     `)
 basic.pause(100)
 basic.forever(function () {
-    led.plot(randint(0, 4), randint(0, 4))
+    led.plotBrightness(randint(0, 4), randint(0, 4), randint(0, 255))
     basic.pause(randint(0, 20))
     led.unplot(randint(0, 4), randint(0, 4))
     basic.pause(randint(0, 20))
+    led.toggle(randint(0, 4), 0)
 })
