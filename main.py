46235674cd8d9073f82d28537797314e49526d6f
dsssssssss9 <30810847+dsssssssss9@users.noreply.github.com>
2020-10-14 23:54:47 +0100

    set display to greyscale mode

--- main.py
+++ main.py
@@ -1,17 +1,18 @@
-led.setDisplayMode(DisplayMode.Greyscale)
-basic.clearScreen()
-basic.showLeds(`
+led.set_display_mode(DisplayMode.GREYSCALE)
+basic.clear_screen()
+basic.show_leds("""
     # # # # #
     # # # # #
     # # # # #
     # # # # #
     # # # # #
-    `)
+    """)
 basic.pause(100)
-basic.forever(function () {
-    led.plotBrightness(randint(0, 4), randint(0, 4), randint(0, 255))
+
+def on_forever():
+    led.plot_brightness(randint(0, 4), randint(0, 4), randint(0, 255))
     basic.pause(randint(0, 20))
     led.unplot(randint(0, 4), randint(0, 4))
     basic.pause(randint(0, 20))
     led.toggle(randint(0, 4), 0)
-})
+basic.forever(on_forever)
